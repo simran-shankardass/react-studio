353d46e3cbaa53ea9d824356805e55a46c5b276a
Simran Shankardass <simranshankardass@Simrans-MacBook-Pro.local>
2022-11-09 18:47:04 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -12,17 +13,32 @@ function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
 
+  //const [count, setCount] = useState(0);
+
+  const [cart, setCart] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const addToCart = (itemName, itemPrice) => {
+    console.log(cart);
+    setCart([...cart, itemName])
+    setTotalPrice(totalPrice + itemPrice);
+  }
+
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        //<p>Bakery Item {index}</p> // replace with BakeryItem component
+       <BakeryItem prop1={item} updateCart={addToCart}/>
       ))}
+
+    
 
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        {cart}
+        <p>Total Price = {totalPrice}</p>
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,16 @@
 // TODO: create a component that displays a single bakery item
+
+function BakeryItem({prop1, updateCart}) {
+    return (
+        <div>
+            <h3>{prop1.name}</h3>
+            <img src={prop1.image} />
+            <p>{prop1.description}</p>
+            <p>{prop1.price}</p>
+            <button onClick={ () => updateCart(prop1.name, prop1.price)}>Add to Cart</button>
+        </div>
+    )
+}
+
+
+export default BakeryItem;
